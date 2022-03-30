5bebff5234a80b623ada447ca75eb96b5b39817e
JoA Azria <joa.azria@gmail.com>
2022-03-30 22:37:57 +0200

    mock up v1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :laurence, :pourqui, :contact]
 
   def home
   end
+
+  def laurence
+  end
+
+  def pourqui
+  end
+
 end
